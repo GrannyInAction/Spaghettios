b15f6b60eff73819149880d26f9e9158347ad741
Iz <srlbtncs@gmail.com>
2014-06-26 00:24:41 -0400

    ooh oh

--- MedMon/src/com/example/medmon/MainActivity.java
+++ MedMon/src/com/example/medmon/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.medmon;
 
+import com.example.patient.MainActivity;
+import com.example.patient.NextActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +17,13 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button b = (Button) findViewById(R.id.button1);
+		b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this, NextActivity.class);
+            startActivity(i);
+            }
+        });
+    }
 	}
-}
+
--- MedMon/src/com/example/medmon/NextActivity.java
+++ MedMon/src/com/example/medmon/NextActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class NextActivity extends Activity {
 
@@ -11,5 +13,6 @@ public class NextActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_next);
+		
 	}
 }
