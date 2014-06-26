a02978ce123633df709845b297600271473e37f0
Iz <srlbtncs@gmail.com>
2014-06-26 01:14:51 -0400

    erhh

--- MedMon/src/com/example/medmon/NextActivity.java
+++ MedMon/src/com/example/medmon/NextActivity.java
@@ -1,18 +1,24 @@
 package com.example.medmon;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class NextActivity extends Activity {
-
+	
+	TimePickerDialog timePicker1 = null;
+	private EditText time;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_next);
-		
+
+
 	}
 }
